ea9d1a8080ace98473a685da0a98e3743a284419
PhanVanHuy8888 <hphan0859@gmail.com>
2024-09-15 14:56:51 +0700

    source

---
+++ src/main/java/com/example/trendtyschool/TrendtySchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.trendtyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendtySchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendtySchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trendtyschool/TrendtySchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trendtyschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendtySchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
