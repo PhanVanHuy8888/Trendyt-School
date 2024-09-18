9fb8177499f580323d4626edde6963dd40966c30
PhanVanHuy8888 <hphan0859@gmail.com>
2024-09-18 21:50:22 +0700

    update

---
+++ src/main/java/com/example/trendtyschool/config/ConfigCors.java
@@ -0,0 +1,17 @@
+package com.example.trendtyschool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfigCors implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
